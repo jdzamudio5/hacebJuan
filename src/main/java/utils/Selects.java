f8fba57e343133a382e26a648f95dfe99d217972
Juan Zamudio <jdzamudio5@gmail.com>
2023-12-06 17:19:13 -0500

    x

--- src/main/java/utils/Selects.java
+++ src/main/java/utils/Selects.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.Select;
 public class Selects {
     public static void select(WebElementFacade webElementFacade, String data){
         Select select = new Select(webElementFacade);
+
         select.selectByVisibleText(data);
 
     }
